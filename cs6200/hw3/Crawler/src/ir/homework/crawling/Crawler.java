091f4a2a3c736fbce3cd1ab56a41b99758743254
Wei Luo <luowei@ccs.neu.edu>
2015-03-19 01:26:49 -0400

    CS6200 IR HW3 Crawling without elasticsearch

--- cs6200/hw3/Crawler/src/ir/homework/crawling/Crawler.java
+++ cs6200/hw3/Crawler/src/ir/homework/crawling/Crawler.java
@@ -1,21 +1,110 @@
 package ir.homework.crawling;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Information Retrieval Homework
  * Created by Wei Luo on 3/18/15.
  */
 public class Crawler {
-    private static final String[] seeds = {
+    public static final String[] SEEDS = {
             "http://en.wikipedia.org/wiki/List_of_terrorist_incidents",
             "http://en.wikipedia.org/wiki/September_11_attacks",
             "http://en.wikipedia.org/wiki/American_Airlines_Flight_77",
             "http://en.wikipedia.org/wiki/World_Trade_Center",
             "http://en.wikipedia.org/wiki/Collapse_of_the_World_Trade_Center"
     };
+    public static final String KEYWORD = "terrorism";
+    public static final int MAX_DOCS = 500;
+
+    private Frontier frontier;
+    private Set<String> crawled;
+
+    public Crawler(){
+        frontier = new Frontier();
+        for(String s : SEEDS){
+            frontier.add(s);
+        }
+        crawled = new HashSet<String>();
+    }
+
+    public void crawl(){
+        while(!frontier.empty() && crawled.size() < MAX_DOCS){
+            String url = frontier.next();
+            Set<String> newUrls = getLinksFromURL(url);
+            if(newUrls != null) {
+                System.out.println("Crawling "+url);
+                for (String nu : newUrls) {
+                    if(!crawled.contains(nu)) {
+                        frontier.add(nu);
+                    }
+                }
+                crawled.add(url);
+            }
+        }
+        System.out.println("Documents crawled: " + crawled.size());
+    }
+
+    public static Set<String> getLinksFromURL(String url) {
+        Set<String> urls = new HashSet<String>();
+        try {
+            Document doc = Jsoup.connect(url).get();
+            if (!doc.html().contains(KEYWORD)) {
+                // no need to crawl
+                return null;
+            }
+            Elements newsHeadlines = doc.select("a[href]");
+            for (Element link : newsHeadlines) {
+                String linkURL = link.attr("abs:href");
+                urls.add(canonicalizeURL(linkURL));
+            }
+
+        } catch (IOException e) {
+            // url could not be opened
+            return null;
+        } catch (URISyntaxException e){
+            // url could not be opened
+            return null;
+        }
+        return urls;
+    }
+
+    private static String canonicalizeURL(String url) throws URISyntaxException, MalformedURLException {
+
+        URL curl = new URL(url);
+        // Convert the scheme and host to lower case
+        String protocol = curl.getProtocol().toLowerCase();
+        String host = curl.getHost().toLowerCase();
+        // Remove port 80 from http URLs, and port 443 from HTTPS URLs
+        int port = curl.getPort();
+        if (port == curl.getDefaultPort()) {
+            port = -1;
+        }
+        String path = curl.getPath();
+        path = new URI(path).normalize().toString();
+        // Remove duplicate slashes
+        while(path.contains("//")){
+            path = path.replace("//","/");
+        }
+        path = path.trim();
+        url = new URL(protocol, host, port, path).toString();
+
+        return url;
+    }
 
     public static void main(String[] args){
-        for(String s : seeds) {
-            System.out.println(s);
-        }
+        Crawler crawler = new Crawler();
+        crawler.crawl();
     }
 }
---
+++ cs6200/hw3/Crawler/src/ir/homework/crawling/Frontier.java
@@ -0,0 +1,76 @@
+package ir.homework.crawling;
+
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * Created by luowei on 3/18/15.
+ */
+public class Frontier {
+    private HashMap<Integer,Set<String>> frontier;
+    private HashMap<String,Integer> urlsMap;
+    private int maxCount;
+
+    public Frontier(){
+        frontier = new HashMap<Integer, Set<String>>();
+        urlsMap = new HashMap<String, Integer>();
+        maxCount = 0;
+    }
+
+    // get the next url to be crawled in the frontier
+    public String next(){
+        if(empty()){
+            return "Frontier Empty!";
+        }
+        Set<String> list = frontier.get(maxCount);
+        if(list.size()==0){
+            frontier.remove(maxCount);
+            maxCount--;
+            return next();
+        }
+        String next = list.iterator().next();
+        urlsMap.remove(next);
+        list.remove(next);
+        if(list.size() == 0){
+            frontier.remove(maxCount);
+            maxCount--;
+        } else {
+            frontier.put(maxCount,list);
+        }
+        return next;
+    }
+
+    public boolean empty(){
+        return maxCount == 0;
+    }
+
+    // add a new url to the frontier
+    public void add(String url){
+        int level = 0;
+        Set<String> oldList;
+        Set<String> newList;
+        if(urlsMap.containsKey(url)){
+            // get the level from frontier
+            level = urlsMap.get(url);
+        }
+        if(level > 0){
+            // remove it from old level list
+            oldList = frontier.get(level);
+            oldList.remove(url);
+            frontier.put(level,oldList);
+        }
+        // get the new level list
+        if(frontier.containsKey(level+1)){
+            newList = frontier.get(level+1);
+        } else {
+            newList = new LinkedHashSet<String>();
+            maxCount++;
+        }
+        // add it to the new level list
+        newList.add(url);
+        frontier.put(level+1,newList);
+        // update its level
+        urlsMap.put(url,level+1);
+    }
+}
