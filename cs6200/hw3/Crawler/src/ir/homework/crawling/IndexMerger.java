36f52a28659f95d4e7263a1fd71add7b90a724f1
Wei Luo <luowei@ccs.neu.edu>
2015-04-01 19:35:24 -0400

    CS6200 IR Hw3 remove multi thread memory eating

--- cs6200/hw3/Crawler/src/ir/homework/crawling/IndexMerger.java
+++ cs6200/hw3/Crawler/src/ir/homework/crawling/IndexMerger.java
@@ -38,36 +38,13 @@ public class IndexMerger {
     private static final String TO_INDEX_NAME = "crawler_data";
     private static final String DOC_TYPE = "document";
 
-    private boolean merging;
-    private ConcurrentLinkedQueue<ESElement> docQueue;
-    private MergerThread[] mergeThreads;
-
     public IndexMerger() {
         node = NodeBuilder.nodeBuilder().node();
         client = node.client();
-        merging = true;
-        docQueue = new ConcurrentLinkedQueue<ESElement>();
-        mergeThreads = new MergerThread[5];
     }
 
     public void releaseNode(){
         node.close();
-    }
-
-    public void merge() {
-        for(int i = 0; i < mergeThreads.length; i++){
-            mergeThreads[i] = new MergerThread();
-            mergeThreads[i].start();
-        }
-        loadDocuments();
-        merging = false;
-        try {
-            for (int i = 0; i < mergeThreads.length; i++) {
-                mergeThreads[i].join();
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 
     private void loadDocuments() {
@@ -79,7 +56,7 @@ public class IndexMerger {
         while(true) {
             for (SearchHit hit : scrollResp.getHits().getHits()) {
                 ESElement ese = new ESElement(hit);
-                docQueue.add(ese);
+                merge(ese);
             }
             scrollResp = client.prepareSearchScroll(scrollResp.getScrollId())
                     .setScroll(new TimeValue(600000)).execute().actionGet();
@@ -90,66 +67,55 @@ public class IndexMerger {
         }
     }
 
-    private class MergerThread extends Thread {
-        @Override
-        public void run() {
-            ESElement ese = docQueue.poll();
-            while (merging || ese != null) {
-                if(ese != null) {
-                    QueryBuilder qb = idsQuery().ids(ese.getId());
-                    SearchResponse searchResp = client.prepareSearch(TO_INDEX_NAME)
-                            .setQuery(qb).execute().actionGet();
-                    if(searchResp.getHits().totalHits() > 0){
-                        updateIndex(ese);
-                    } else{
-                        System.out.println("Indexing " + ese.getId());
-                        IndexResponse indexResp = null;
-                        while(indexResp == null || !indexResp.getId().equals(ese.getId())){
-                            // ensure success index creation
-                            indexResp = client.prepareIndex(TO_INDEX_NAME, DOC_TYPE, ese.getId())
-                                    .setSource(ese.getBuilder()).execute().actionGet();
-                        }
-                    }
-                }
-                ese = docQueue.poll();
+    private void merge(ESElement ese){
+        QueryBuilder qb = idsQuery().ids(ese.getId());
+        SearchResponse searchResp = client.prepareSearch(TO_INDEX_NAME)
+                .setQuery(qb).execute().actionGet();
+        if(searchResp.getHits().totalHits() > 0){
+            updateIndex(ese);
+        } else{
+            System.out.println("Indexing " + ese.getId());
+            IndexResponse indexResp = null;
+            while(indexResp == null || !indexResp.getId().equals(ese.getId())){
+                // ensure success index creation
+                indexResp = client.prepareIndex(TO_INDEX_NAME, DOC_TYPE, ese.getId())
+                        .setSource(ese.getBuilder()).execute().actionGet();
             }
         }
+    }
 
-        private void updateIndex(ESElement ese) {
-            System.out.println("Updating " + ese.getId());
-            StringBuffer sb = new StringBuffer();
-            if(ese.getInlinks().size() > 0) {
-                sb.append("ctx._source.inlinks");
-                for(String in : ese.getInlinks()){
-                    sb.append("<<"+"\""+in+"\"");
-                }
-                sb.append("; ctx._source.inlinks=ctx._source.inlinks.unique(); ");
+    private void updateIndex(ESElement ese) {
+        System.out.println("Updating " + ese.getId());
+        StringBuffer sb = new StringBuffer();
+        if(ese.getInlinks().size() > 0) {
+            sb.append("ctx._source.inlinks");
+            for(String in : ese.getInlinks()){
+                sb.append("<<"+"\""+in+"\"");
             }
-            if(ese.getOutlinks().size() > 0) {
-                sb.append("ctx._source.outlinks");
-                for(String out : ese.getOutlinks()){
-                    sb.append("<<"+"\""+out+"\"");
-                }
-                sb.append("; ctx._source.outlinks = ctx._source.outlinks.unique(); ");
-            }
-            String script = sb.toString();
-            if(script != ""){
-                try {
-                    client.prepareUpdate(TO_INDEX_NAME, "document", ese.getId())
-                            .setScript(script, ScriptService.ScriptType.INLINE)
-                            .execute().actionGet();
-                } catch (VersionConflictEngineException e) {
-                    // update conflict save for retry
-                    docQueue.add(ese);
-                    e.printStackTrace();
-                }
+            sb.append("; ctx._source.inlinks=ctx._source.inlinks.unique(); ");
+        }
+//            if(ese.getOutlinks().size() > 0) {
+//                sb.append("ctx._source.outlinks");
+//                for(String out : ese.getOutlinks()){
+//                    sb.append("<<"+"\""+out+"\"");
+//                }
+//                sb.append("; ctx._source.outlinks = ctx._source.outlinks.unique(); ");
+//            }
+        String script = sb.toString();
+        if(script != ""){
+            try {
+                client.prepareUpdate(TO_INDEX_NAME, "document", ese.getId())
+                        .setScript(script, ScriptService.ScriptType.INLINE)
+                        .execute().actionGet();
+            } catch (VersionConflictEngineException e) {
+                e.printStackTrace();
             }
         }
     }
 
     public static void main(String[] args){
         IndexMerger im = new IndexMerger();
-        im.merge();
+        im.loadDocuments();
         im.releaseNode();
     }
 }
